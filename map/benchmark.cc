c40fdee409dd60df3e5ae6e8cdf67e39d16e7535
Hiroki KUMAZAKI <rintyo_@hotmail.com>
2012-10-15 20:32:47 +0900

    use dense map in benchmark

--- map/benchmark.cc
+++ map/benchmark.cc
@@ -5,7 +5,7 @@
 #include <tr1/unordered_map>
 #include <boost/random.hpp>
 #include <google/sparse_hash_map>
-//#include <google/dense_hash_map>
+#include <google/dense_hash_map>
 #include "map.hpp"
 using namespace boost::chrono;
 using namespace nanahan;
@@ -55,34 +55,35 @@ void measure_speed(const std::string& name,TargetType& target, size_t num, eval_
 
 int main(void)
 {
-  for(int i =  1000000; i <= 10000000; i += 1000000){
+  for(int i =  100000; i <= 10000000; i += 100000){
     for(int j =0; j<10; ++j){
       boost::unordered_map<int, int> bmap;
       Map<int, int> nmap;
       std::tr1::unordered_map<int, int> tmap;
       sparse_hash_map<int, int> gsmap;
-      //dense_hash_map<int, int> gdmap;
+      dense_hash_map<int, int> gdmap;
+      gdmap.set_empty_key(NULL);
 
       std::cout << "insert:" << std::endl;
       measure_speed("nanahan",nmap, i, insert);
       measure_speed("boost  ",bmap, i, insert);
       measure_speed("tr1    ",tmap, i, insert);
       measure_speed("g_sparse",gsmap, i, insert);
-      //measure_speed("g_dense",gdmap, i, insert);
+      measure_speed("g_dense",gdmap, i, insert);
 
       std::cout << "find:" << std::endl;
       measure_speed("nanahan",nmap, i, find);
       measure_speed("boost  ",bmap, i, find);
       measure_speed("tr1    ",tmap, i, find);
       measure_speed("g_sparse",gsmap, i, find);
-      //measure_speed("g_dense",gdmap, i, find);
+      measure_speed("g_dense",gdmap, i, find);
 
       std::cout << "erase:" << std::endl;
       measure_speed("nanahan",nmap, i, erase);
       measure_speed("boost  ",bmap, i, erase);
       measure_speed("tr1    ",tmap, i, erase);
       measure_speed("g_sparse",gsmap, i, erase);
-      //measure_speed("g_dense",gdmap, i, erase);
+      measure_speed("g_dense",gdmap, i, erase);
     }
   }
 }
